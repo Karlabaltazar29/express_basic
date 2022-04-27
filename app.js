7c485985a1607a4e5a407ebb51cab5862012e76b
karla baltazar <baltazarkarla9@gmail.com>
2022-04-27 14:24:50 -0500

    creacion de express basic

---
+++ app.js
@@ -0,0 +1,36 @@
+ // Usando objeto express
+const express = require('express')
+const app = express () // App de Express
+// Puerto en que vamos a ver nuestra app: localhost: 3000
+const port = 3000
+
+// path inicial, responderá a la url localhost: 3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//Respondiendo texto
+//localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+// Regresando un objeto
+//localhost:3000/explorersInNode
+
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
+// Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/Karla
+//req.params = {"explorerName": "Karla"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
+// Con esto inicializamos esta app
+app.listen (port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
